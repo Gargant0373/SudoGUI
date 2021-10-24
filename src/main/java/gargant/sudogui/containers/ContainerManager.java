31c55d6053fd3204175fc9575016ca8f7fb1740d
Gargant <gargant0373@gmail.com>
2021-10-24 13:30:23 +0300

    Commented code and added delete and get by name methods.

--- src/main/java/gargant/sudogui/containers/ContainerManager.java
+++ src/main/java/gargant/sudogui/containers/ContainerManager.java
@@ -8,19 +8,77 @@ import masecla.mlib.main.MLib;
 public class ContainerManager {
 
 	private MLib lib;
-	
+
 	public ContainerManager(MLib lib) {
 		this.lib = lib;
 	}
-	
+
+	// TODO: Add SQL support
+	/**
+	 * Method used to fetch the containers from the configuration file.
+	 * It has no safety checks in place for containers so some of them might be corrupted
+	 * if config was edited manually.
+	 * 
+	 * @return a list of containers from config.
+	 */
 	@SuppressWarnings("unchecked")
 	public List<SerializeableContainer> getContainers() {
-		return (List<SerializeableContainer>) lib.getConfigurationAPI().getConfig("containers").get("guis", new ArrayList<>());
+		return (List<SerializeableContainer>) lib.getConfigurationAPI().getConfig("containers").get("guis",
+				new ArrayList<>());
 	}
 	
+	// TODO: Add SQL support.
+	/**
+	 * Method used to update the list of containers in the storing prefference.
+	 * 
+	 * @param Containers - The list of registered containers.
+	 */
+	private void updateContainerList(List<SerializeableContainer> containers) {
+		lib.getConfigurationAPI().getConfig("containers").set("guis", containers);
+	}
+
+	/**
+	 * Method used to register a container to the config.
+	 * 
+	 * @param Container - the container you want to register.
+	 */
 	public void registerContainer(SerializeableContainer cont) {
 		List<SerializeableContainer> containers = this.getContainers();
 		containers.add(cont);
-		lib.getConfigurationAPI().getConfig("containers").set("guis", containers);
+		this.updateContainerList(containers);
+	}
+
+	/**
+	 * Method used to find a container by name.
+	 * 
+	 * @param name - Name of the container.
+	 * @return null if no container found or the container that you're looking for
+	 */
+	public SerializeableContainer getContainerByName(String name) {
+		for (SerializeableContainer c : this.getContainers())
+			if (c.getContainerName().equalsIgnoreCase(name))
+				return c;
+		return null;
+	}
+	
+	/**
+	 * Method used to delete a container from the config.
+	 * 
+	 * @param Container - Container you want to remove.
+	 * @return true if container removed, false if it never existed.
+	 */
+	public boolean deleteContainer(SerializeableContainer cont) {
+		if(cont == null)
+			return false;
+		
+		List<SerializeableContainer> registeredContainers = this.getContainers();
+			
+		if(!registeredContainers.contains(cont)) {
+			return false;
+		}
+		
+		registeredContainers.remove(cont);
+		this.updateContainerList(registeredContainers);
+		return true;
 	}
 }
